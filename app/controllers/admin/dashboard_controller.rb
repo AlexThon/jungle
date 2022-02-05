590401b28329492864103e9ce2d2da98acb2d5b9
Alex Thon <ghaiquol@gmail.com>
2022-02-04 21:45:05 -0500

    Add counts to dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
   def show
+    categories = Category.all
+    products = Product.all
+  
+    @products_count = products.count
+    @categories_count = categories.count
+   
   end
 end
